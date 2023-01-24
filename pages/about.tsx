defb6b11dda98c9bd21eaaefd41a6a640bb036fd
tihonenko <nikireatz@gmail.com>
2023-01-24 14:53:12 +0300

    correct

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,16 @@
+import { NextPage } from 'next';
 import Photo_about from '../app/components/about/photo';
 import Hero_about from '../app/components/about/hero';
 import Work_about from '../app/components/about/work';
 
-const About = ()=>{
-    return(
-        <div className='container'>
-            <Hero_about/>
-            <Photo_about/>
-            <Work_about/>
-        </div>
-    )
-}
+const About: NextPage = () => {
+	return (
+		<section className='container'>
+			<Hero_about />
+			<Photo_about />
+			<Work_about />
+		</section>
+	);
+};
 
-export default About
+export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next';
-import Header from '../app/components/header/Header';
 
 const Home: NextPage = () => {
 	return <div>HEllO WORLD!!</div>;
