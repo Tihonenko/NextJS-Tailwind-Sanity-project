f9f943cf1a0ebd971b1ca00383a9d3e56b285ab2
vlad <vlad.kletsky@yandex.by>
2023-01-29 18:52:41 +0300

    .

---
+++ app/components/become-page/becomeHero.tsx
@@ -0,0 +1,13 @@
+import styles from './become.module.scss'
+
+const HeroBecome = () => {
+  return (
+    <section className={styles.become_hero}>
+      <p className={styles.marquee}>
+        BECOME A MODEL BECOME A MODEL
+      </p>
+    </section>
+  )
+}
+
+export default HeroBecome
---
+++ pages/become.tsx
@@ -0,0 +1,12 @@
+import HeroBecome from "@components/become-page/becomeHero"
+
+
+const become = () => {
+  return (
+    <>
+        <HeroBecome/>
+    </>
+  )
+}
+
+export default become
