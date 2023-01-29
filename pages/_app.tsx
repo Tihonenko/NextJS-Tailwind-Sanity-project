84fc01b57a0cbbffbc49dba6afa54718222c7ff8
tihonenko <nikireatz@gmail.com>
2023-01-29 23:46:38 +0300

    start hook, animation edit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import { store } from '@app/store/store';
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 
-import Layout from '../app/components/layout/Layout';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
